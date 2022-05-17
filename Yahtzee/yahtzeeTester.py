ac6c4357fa16d7c701244ae795a9d1f46ed79278
RobertBehring <behringr@oregonstate.edu>
2022-05-16 21:01:03 -0700

    05/16 - Working on it! A little bit on the tester

--- Yahtzee/yahtzeeTester.py
+++ Yahtzee/yahtzeeTester.py
@@ -4,8 +4,15 @@ import yahtzeeScoreCard as ScoreCard
 
 class MyTestCase(unittest.TestCase):
     def test_scoreboards(self):
-        four_player_cards = ScoreCard.YahtzeeScoreCard(4)
-        self.assertEqual(True, False)  # add assertion here
+        yahtzee_score_cards = ScoreCard.YahtzeeScoreCard(4)
+        player1_card = yahtzee_score_cards.determine_player_card(1)
+        player2_card = yahtzee_score_cards.determine_player_card(2)
+        player3_card = yahtzee_score_cards.determine_player_card(3)
+        player4_card = yahtzee_score_cards.determine_player_card(4)
+        self.assertEqual(player1_card[0]['player number'], 1)
+        self.assertEqual(player2_card[0]['player number'], 2)
+        self.assertEqual(player3_card[0]['player number'], 3)
+        self.assertEqual(player4_card[0]['player number'], 4)
 
 
 if __name__ == '__main__':
